5ec8f48239a1a61f7fd701c10eabc366e1021ea3
Kaliane <kaliane.pemi@gmail.com>
2021-11-23 22:12:58 -0300

    recycler view

--- app/src/main/java/com/example/words/MainActivity.kt
+++ app/src/main/java/com/example/words/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.words
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.words.adapter.LetterAdapter
+import com.example.words.databinding.ActivityMainBinding
+
+//import com.example.words.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager (this)
+        recyclerView.adapter = LetterAdapter ()
     }
 }
